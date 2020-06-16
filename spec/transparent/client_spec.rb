81e196ede9907aff5f56839362fb67d4aa1dc949
Igor <igor@getlavanda.com>
2020-06-16 12:18:00 +0100

    [feature] Add `combined` which requests listings/aggregated data

--- spec/transparent/client_spec.rb
+++ spec/transparent/client_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Transparent::Client do
       it 'requests aggregated pricing via http' do
         expected_http_req
 
-        described_class.new.aggregated(req_params)
+        described_class.new(**req_params).aggregated
 
         assert_requested(expected_http_req)
       end
@@ -44,7 +44,7 @@ RSpec.describe Transparent::Client do
           expected_http_req
 
           expect(
-            described_class.new.aggregated(req_params)
+            described_class.new(**req_params).aggregated
           ).to be_nil
         end
       end
@@ -55,9 +55,77 @@ RSpec.describe Transparent::Client do
         it 'fetches aggregated pricing data' do
           expected_http_req
 
-          expect(described_class.new.aggregated(req_params)).to eq(
+          expect(described_class.new(**req_params).aggregated).to eq(
             adr: 318,
             occupancy: 0.36000001430511475
+          )
+        end
+      end
+    end
+  end
+
+  context '#combined' do
+    context 'when API returns OK responses' do
+      let(:listings_body) { '' }
+      let(:listings_http_status) { 200 }
+      let(:expected_listings_http_req) do
+        stub_request(
+          :get,
+          Transparent::Constants::BASE_URI + Transparent::Constants::ENDPOINT_URIS[:listings]
+        ).with(
+          query: req_params,
+          headers: {
+            'Expect' => '',
+            'User-Agent' => 'Typhoeus - https://github.com/typhoeus/typhoeus'
+          }
+        ).to_return(status: listings_http_status, body: listings_body, headers: {})
+      end
+
+      let(:aggregated_body) { '' }
+      let(:aggregated_http_status) { 200 }
+      let(:expected_aggregated_http_req) do
+        stub_request(
+          :get,
+          Transparent::Constants::BASE_URI + Transparent::Constants::ENDPOINT_URIS[:aggregated]
+        ).with(
+          query: req_params,
+          headers: {
+            'Expect' => '',
+            'User-Agent' => 'Typhoeus - https://github.com/typhoeus/typhoeus'
+          }
+        ).to_return(status: aggregated_http_status, body: aggregated_body, headers: {})
+      end
+
+      before do
+        expected_listings_http_req && expected_aggregated_http_req
+      end
+
+      it 'requests aggregated and listings pricing via http' do
+        described_class.new(**req_params).combined
+
+        assert_requested(expected_listings_http_req)
+        assert_requested(expected_aggregated_http_req)
+      end
+
+      context 'when API returns empty body responses' do
+        it 'returns nil' do
+          expect(
+            described_class.new(**req_params).combined
+          ).to be_nil
+        end
+      end
+
+      context 'when API returns contentful responses' do
+        let(:aggregated_body) { File.read('spec/simulated_api_responses/dataful/aggregated.json') }
+        let(:listings_body) { File.read('spec/simulated_api_responses/dataful/listings.json') }
+
+        it 'fetches combined pricing data' do
+          expect(described_class.new(**req_params).combined).to eq(
+            adr: 318,
+            occupancy: 0.36000001430511475,
+            listings: [
+              { adr: 112.98418972332016, occupancy: 0.36 }
+            ]
           )
         end
       end
